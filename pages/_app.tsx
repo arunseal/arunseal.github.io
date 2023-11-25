f58072e446ac4652c9f8517ae331da0dec8f73a4
Arunava Seal <arunava@Arunavas-MacBook-Air.local>
2023-11-25 20:44:48 +0530

    fix for favicon not showing up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,7 +27,7 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
       <Head>
         <title>Arunava Seal</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link rel="shortcut icon" href="/images/rivian.svg" />
+        <link rel="shortcut icon" href="images/rivian.svg" />
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
